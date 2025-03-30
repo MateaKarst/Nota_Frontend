5e46d8a4b25c6b3f977ba22aeef0f45cfbc7aea3
Petra712 <pluckova1@gmail.com>
2025-03-30 18:38:23 +0200

    Adding login pages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
 import NotaLogo from './components/LogoNota';
 import MainButton from './components/MainButton';
+import CreateAccountPage from './pages/CreateAccountPage';
+import LoginPage from './pages/LoginPage';
 
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <NotaLogo colorIndex={3} /> {/* Помаранчеве лого */}
-      <NotaLogo colorIndex={1} /> {/* Фіолетове лого */}
-      <NotaLogo colorIndex={1} /> {/* Фіолетовий логотип */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-        <MainButton /> 
-      </div>
-      </header>
-    </div>
-  );
+  return <CreateAccountPage />;
 }
   export default App ;
