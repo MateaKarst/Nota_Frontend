33a4786d2b325344312ae5772487cacf6711d1f2
DrobiazkoSofiia <sofiya.drobyazko@gmail.com>
2025-04-01 19:11:40 +0200

    Heder4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import NotaLogo from './components/LogoNota';
 import HeaderMain from './components/header-main';
 import HeaderProfile from './components/header-profile';
+import HeaderMySongs from './components/header-mysongs';
+import HeaderVariants from './components/header-backarrow';
 
 function App() {
   return (
@@ -17,6 +19,13 @@ function App() {
       <NotaLogo colorIndex={1} /> 
       <HeaderMain /> 
       <HeaderProfile />
+      <HeaderMySongs />
+      <HeaderVariants mode={"text"} />
+      <HeaderVariants mode={"edit"} />
+      <HeaderVariants mode={"menu"} />
+      <HeaderVariants mode={"black text"} />
+      <HeaderVariants mode={"default"} />
+      <HeaderVariants mode={"user"} />
         <a
           className="App-link"
           href="https://reactjs.org"
