28ac2711ff62c67b86c333590f2c6d09684443ef
dominykam <92014176+dominykam@users.noreply.github.com>
2025-04-06 16:01:01 +0200

    Added editor instrument component

--- src/App.js
+++ src/App.js
@@ -6,29 +6,27 @@ import MainButton from './components/MainButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <NotaLogo colorIndex={3} /> {/* Помаранчеве лого */}
-      <NotaLogo colorIndex={1} /> {/* Фіолетове лого */}
-      <NotaLogo colorIndex={1} /> {/* Фіолетовий логотип */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-        <MainButton /> 
-      </div>
-      </header>
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <NotaLogo colorIndex={3} /> {/* Помаранчеве лого */}
+  <NotaLogo colorIndex={1} /> {/* Фіолетове лого */}
+  <NotaLogo colorIndex={1} /> {/* Фіолетовий логотип */}
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+    <div>
+    <MainButton /> 
+  </div>
+  </header>
 
-    </div>
   );
 }
   export default App ;
