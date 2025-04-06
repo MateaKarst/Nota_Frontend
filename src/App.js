ea319f309ef8641ea0c6ec849d786369e725555a
MateaKarst <matea.karst@gmail.com>
2025-04-06 04:01:56 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-
-
-import HeaderMain from './components/header-main';
-import HeaderProfile from './components/header-profile';
-import HeaderMySongs from './components/header-mysongs';
-import HeaderVariants from './components/header-backarrow';
-
-=======
->>>>>>> development
-import NotaLogo from './components/Logos/LogoNota';
-import Buttons from './components/Buttons/BasicBtn';
-import { ReactComponent as Logo } from "./logo.svg";
-import AttachFileBtn from './components/Buttons/AttachFileBtn'
-import ProBtn from './components/Buttons/ProBtn'
-import ConnectBtn from './components/Buttons/ConnectBtn';
-import PurpleTagsButton from './components/Tags/PurpleTag';
-
-<<<<<<< HEAD
-=======
-
->>>>>>> development
 
 function App() {
   return (
