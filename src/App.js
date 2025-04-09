948ca530514b30245dee3f69073af814ef3963fb
MateaKarst <matea.karst@gmail.com>
2025-04-10 06:13:23 +0800

    timer completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import Timer from './components/Editor/Timer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Timer variation={2} />
       </header>
     </div>
   )
