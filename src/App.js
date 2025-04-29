79521693dadaa2f1ffa0796c7655d2d81e11086e
MateaKarst <matea.karst@gmail.com>
2025-04-29 15:19:16 +0200

    cleaned app page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import SmallCard from './components/MusicCard/SmallCard/SmallCard';
-import MusicCard from './components/MusicCard/HomeAndMySongsCards/MusicCard';
-import CaroselCard from './components/MusicCard/CaroselCard/CaroselCard';
 import '../src/App.css'
+
 function App() {
   return (
     <div className="App">
-      
-         <MusicCard
-          imageUrl={"https://img.freepik.com/premium-vector/beautiful-calm-night-mountain-with-moonlight_104785-1378.jpg"}
-          title="Lofi Beats"
-          creator="DJ Chill"
-          contributersNbr = "3"
-          layout="row" 
-        /> 
-        <MusicCard
-          imageUrl={"https://img.freepik.com/premium-vector/beautiful-calm-night-mountain-with-moonlight_104785-1378.jpg"}
-          title="Lofi Beats"
-          creator="DJ Chill"
-          contributersNbr = "3"
-          layout="row"
-        /> 
-          <MusicCard
-          imageUrl={"https://img.freepik.com/premium-vector/beautiful-calm-night-mountain-with-moonlight_104785-1378.jpg"}
-          title="Lofi Beats"
-          creator="DJ Chill"
-          contributersNbr = "3"
-          layout="row"
-        /> 
-          <MusicCard
-          imageUrl={"https://img.freepik.com/premium-vector/beautiful-calm-night-mountain-with-moonlight_104785-1378.jpg"}
-          title="Lofi Beats"
-          creator="DJ Chill"
-          contributersNbr = "3"
-          layout="row"
-        /> 
+      <header className="App-header">
+      </header>
     </div>
   );
 }
