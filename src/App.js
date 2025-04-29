bdd312d7e6a25e75567939fac2da73c72a619c83
DrobiazkoSofiia <sofiya.drobyazko@gmail.com>
2025-04-29 22:46:06 +0200

    song-interaction-function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css'
+import LikeButton from '../src/components/like-button.jsx'
+import CommentButton from '../src/components/comment-button.jsx'
+import ShareButton from '../src/components/share-button.jsx'
+import SongInteraction from '../src/components/song-interaction.jsx'
+import LoadingProgress from '../src/components/progressbar.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className="App" style={{backgroundColor:"#000000"}}>
+      
+      <SongInteraction/>
+      <LoadingProgress label="Importing file..." duration={4000} />
+
     </div>
   );
 }
