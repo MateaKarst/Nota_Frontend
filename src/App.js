99e566aa88e7c3366c79a76a0362147c56f961c1
dominykam <92014176+dominykam@users.noreply.github.com>
2025-05-04 17:18:21 +0200

    fixed the issue in chat overview page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomeCarousel from '../src/components/Home/HomeCarousel';
 import CaroselCard from '../src/components/MusicCard/CaroselCard/CaroselCard'
 
+
 function App() {
   return (
     <div className="App">
