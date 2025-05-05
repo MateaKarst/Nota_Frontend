e83c1b35cecb905179e94b49518ada8da9362793
DrobiazkoSofiia <sofiya.drobyazko@gmail.com>
2025-05-05 22:17:50 +0200

    HomePage2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-
-import SmallCard from './components/MusicCard/SmallCard/SmallCard';
-import NavBar from './components/Navigation/NavBar';
 import '../src/App.css'
 import HomePage from './pages/home-page';
 function App() {
