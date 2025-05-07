ed92140b30304d8bc4ee8989db6ec4f6068f387f
VioletaTaneva <violeta.akaunt11@gmail.com>
2025-05-07 13:09:33 +0200

    changed carosel img

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import '../src/App.css';
-//import HomePage from './pages/home-page';
+import HomePage from './pages/home-page';
 import MusicCard from './components/MusicCard/HomeAndMySongsCards/MusicCard';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <HomePage /> */}
+       <HomePage /> 
       
 
- <MusicCard
+ {/* <MusicCard
     imageUrl={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Bachelor%27s_button%2C_Basket_flower%2C_Boutonniere_flower%2C_Cornflower_-_3.jpg/960px-Bachelor%27s_button%2C_Basket_flower%2C_Boutonniere_flower%2C_Cornflower_-_3.jpg"}
     title="Lofi Beats"
     creator="DJ Chill"
@@ -21,7 +21,7 @@ function App() {
     title="Lofi Beats"
     creator="DJ Chill"
     layout="row" 
-  /> 
+  />  */}
     </div>
   );
 }
