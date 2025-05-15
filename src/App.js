b11ed6c12f1479a501cdffa0408ee33d29ec2f2c
Petra712 <pluckova1@gmail.com>
2025-05-15 09:37:33 +0200

    editor page initial upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 
 import '../src/App.css'
+import Editor from '../src/pages/Editor2';
 //import Editor from './pages/editor';
-import Soundwave from './components/Editor/Soundwave';
-import ringtone2 from '../src/assets/editor/iphone-ringtone.mp3'
-import MultitrackMixer from './components/WorkingEditor/MultitrackMixer';
+//import Soundwave from './components/Editor/Soundwave';
+//import ringtone2 from '../src/assets/editor/iphone-ringtone.mp3'
+//import MultitrackMixer from './components/WorkingEditor/MultitrackMixer';
 //import TestingEd from './components/Editor/LibraryMulti/TestingEd';
 
 function App() {
   return (
-    //<Editor/>
+    <Editor/>
     // <Soundwave
     //  audioUrl = {ringtone2}/>
     //<MultiTrackEditor/>
-    <MultitrackMixer />
+    //<MultitrackMixer />
   );
 }
 
---
+++ src/pages/Editor2.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import HeaderVariants from "../components/Headers/HeaderVariants";
+import Timer from '../components/Editor/Timer'
+import MultiTrackMixer from '../components/WorkingEditor/MultitrackMixer'
+
+import '../styles/editor/editor2.css'
+
+
+const Editor = () => {
+    return (
+        <div className="editor-container">
+            <div className="header-block"><HeaderVariants mode="black text" /></div>
+            <div className="timer-block"><Timer variation={1} /></div>
+           <div className="mixer-block"> <MultiTrackMixer></MultiTrackMixer></div>
+    
+        </div>
+    )
+};
+
+export default Editor;
