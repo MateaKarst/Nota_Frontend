cc94fa7e7e3130b93d6a0404e96ac33bbfa864fe
Petra712 <pluckova1@gmail.com>
2025-05-20 10:25:42 +0200

    track upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import '../src/App.css'
- import HomePage from './pages/HomePage';
- import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProfilePage from './pages/ProfilePage';
-import ProfileFriendPage from './pages/ProfileFriendPage';
-import NotificationsPage from './pages/NotificationsPage'
-import ChatOverview from './pages/ChatOverview'
-import MySongsPage from './pages/MySongsPage'
+import Track from "../src/components/Tracks/UserTrack"
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/messages" element={<ChatOverview />} />
-        <Route path="/songs" element={<MySongsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/profilefriend" element={<ProfileFriendPage />} />
-      </Routes>
-    </Router>
+      <Track isOwnTrack={false}/>
     </div>
   );
 }
