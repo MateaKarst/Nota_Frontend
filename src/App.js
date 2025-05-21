19b3d06ed1033b388fd1720675d81ab644f14a43
VioletaTaneva <violeta.akaunt11@gmail.com>
2025-05-21 03:49:38 +0200

    track dropdown added (the track component needs to be updated tho)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import './App.css';
 
 import UploadSong from './pages/UploadSong';
 import TagInput from './components/Tags/TagInput';
+import TrackDropdown from './components/Tracks/TrackDropdown';
 
 
 function App() {
+  const tracks = [
+{ isOwnTrack: true },
+{ isOwnTrack: false },
+{ isOwnTrack: false },
+{ isOwnTrack: true },
+{ isOwnTrack: false },
+{ isOwnTrack: true }, 
+{ isOwnTrack: true }, 
+{ isOwnTrack: true }, 
+{ isOwnTrack: true }, 
+];
   return (
     <div className="App">
-     <UploadSong/>
+     <TrackDropdown tracks={tracks}/>
     </div>
   );
 }
