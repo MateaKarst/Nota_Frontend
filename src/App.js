4d550d6d9b47b2c5530151f11f7926fdc7aa592f
dominykam <92014176+dominykam@users.noreply.github.com>
2025-05-21 13:45:20 +0200

    added song description page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import CreateAccount from "./pages/CreateAccount";
 import ProfilePage from './pages/ProfilePage';
 import ProfileFriendPage from './pages/ProfileFriendPage';
 import NavBar from "./components/Navigation/NavBar";
-import SectionHeadImage from "./components/SongDescription/SectionHeadImage";
 import "./App.css";
 
 function AppContent() {
@@ -30,7 +29,6 @@ function AppContent() {
 
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profilefriend" element={<ProfileFriendPage />} />
-        <Route path="/song" element={<SectionHeadImage />} />
       </Routes>
       {showNavBar && <NavBar />}
     </>
