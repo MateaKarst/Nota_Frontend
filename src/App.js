42f4a9de0087431ff56ce8a64a6641eec7319cc6
VioletaTaneva <violeta.akaunt11@gmail.com>
2025-05-22 10:19:01 +0200

    some commit im not sure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
 
-import UploadSong from './pages/UploadSong';
-import TagInput from './components/Tags/TagInput';
-import TrackDropdown from './components/Tracks/TrackDropdown';
-
-
 function App() {
-  const tracks = [
-{ isOwnTrack: true },
-{ isOwnTrack: false },
-{ isOwnTrack: false },
-{ isOwnTrack: true },
-{ isOwnTrack: false },
-{ isOwnTrack: true }, 
-{ isOwnTrack: true }, 
-{ isOwnTrack: true }, 
-{ isOwnTrack: true }, 
-];
+
   return (
     <div className="App">
-     <TrackDropdown tracks={tracks}/>
+
     </div>
   );
 }
