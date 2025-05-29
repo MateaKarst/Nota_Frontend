aa444d00a23c93f2e2459153e11d367231e14537
VioletaTaneva <violeta.akaunt11@gmail.com>
2025-05-29 15:28:25 +0200

    i acsidently replaced something and now this is fucked

--- src/pages/ProfileFriendPage.jsx
+++ src/pages/ProfileFriendPage.jsx
@@ -9,7 +9,6 @@ import MusicPlayer from '../components/MusicPlayer';
 import API_ENDPOINTS from '../routes/apiEndpoints';
 import { useNavigate } from 'react-router-dom';
 
-
 const ProfileFriendPage = () => {
   const navigate = useNavigate();
 
