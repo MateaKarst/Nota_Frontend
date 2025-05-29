6edbffd22baec74916447bc71f661bb3256fd359
MateaKarst <matea.karst@gmail.com>
2025-05-29 15:35:45 +0200

    backend endpoint:

--- src/routes/apiEndpoints.js
+++ src/routes/apiEndpoints.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://192.168.1.38:3000/api";
-const API_BASE_URL = "http://https://nota-community.netlify.app/api";
+const API_BASE_URL = "https://nota-backend-delta.vercel.app/api";
 
 export const API_ENDPOINTS = {
     AUTH: {
