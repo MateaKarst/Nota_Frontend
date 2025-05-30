6733a4cc3bc3dc2b92af258e5d84fd37ace2dde3
dominykam <92014176+dominykam@users.noreply.github.com>
2025-05-30 13:46:06 +0200

    added progress bars in editor

--- src/pages/Editor2.jsx
+++ src/pages/Editor2.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import HeaderVariants from "../components/Headers/HeaderVariants";
 import Timer from '../components/Editor/Timer'
 import MultiTrackMixer from '../components/WorkingEditor/MultitrackMixer'
 import Button from '../components/Buttons/BasicBtn' 
+import LoadingProgress from "../components/progressbar";
 
 import '../styles/editor/editor2.css'
 
 
 const Editor = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+  const handlePreviewClick = () => {
+    setIsLoading(true);
+    
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  };
+
     return (
         <div className="editor-container">
             <div className="header-block"><HeaderVariants mode="black text" /></div>
             {/*<div className="timer-block"><Timer variation={1} /></div>*/}
            <div className="mixer-block"> <MultiTrackMixer></MultiTrackMixer></div>
           
-            <Button text="Go to Preview" type="medium"></Button>
+           <LoadingProgress label="Loading Preview..." isLoading={isLoading} />
+           <div onClick={handlePreviewClick}>
+            <Button text="Go to Preview" type="medium" />
+        </div>
         </div>
     )
 };
