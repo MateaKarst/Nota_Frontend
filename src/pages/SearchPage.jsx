abf9cf29f41e4f5a4967098b47a7deb6a32e226d
Petra712 <pluckova1@gmail.com>
2025-06-05 12:59:12 +0200

    fixed search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -3,28 +3,40 @@ import HeaderVariants from "../components/Headers/HeaderVariants";
 import SearchBar from "../components/Search/SearchBar";
 import GenreCard from "../components/Search/GenreCard"
 import FilterIcon from "../assets/filter-icon.svg"
+
+import {useNavigate} from 'react-router-dom';
+
 import "../styles/variables.css"
 
 import "../styles/pages/SearchPage.scss"
 
 
 const SearchPage = () => {
+
+    const navigate = useNavigate();
+
+    const handleGenreClick = (genreName) => {
+        navigate ('/view-all', {state: {title: genreName } });
+    };
+
+    const genres = ['jazz', 'rock', 'country', 'metal', 'pop', 'indie'];
+
     return (
         <div className="container">
             <HeaderVariants className="genre-card" mode="default" />
             <div className="search-bar-container">
-            <SearchBar 
-            variant={1}/>
-            <img src={FilterIcon} alt="filter" />
+            <SearchBar  className="search-bar"
+            variant={2}/>
             </div>
-            <div className="genre-box">
-                <GenreCard />
-                <GenreCard mode="country" />
-                <GenreCard mode="metal" />
-                <GenreCard mode="rock" />
-                <GenreCard mode="pop" />
-                <GenreCard mode="indie" />
-            </div>
+            <div className="genre-grid">
+        {genres.map((genre) => (
+          <GenreCard
+            key={genre}
+            mode={genre}
+            onClick={() => handleGenreClick(genre)}
+          />
+        ))}
+      </div>
         </div>
     )
 }
