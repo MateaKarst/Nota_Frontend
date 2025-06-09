ce73245b439a97b74ef5d7bce09fe461f6c82acf
VioletaTaneva <violeta.akaunt11@gmail.com>
2025-06-09 11:19:40 +0200

    mp3 upload only on hp

--- src/components/PopUps/PopUpContent.jsx
+++ src/components/PopUps/PopUpContent.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import AttachFileBtn from '../Buttons/AttachFileBtn';
-import BasicBtn from '../Buttons/BasicBtn';
-import ProBtn from '../Buttons/ProBtn';
-import Record from '../../pages/Record';
+import React, { useState } from "react";
+import AttachFileBtn from "../Buttons/AttachFileBtn";
+import BasicBtn from "../Buttons/BasicBtn";
+import ProBtn from "../Buttons/ProBtn";
+import Record from "../../pages/Record";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-import { ReactComponent as ReportIcon } from '../../assets/icons/report-icon.svg';
+import { ReactComponent as ReportIcon } from "../../assets/icons/report-icon.svg";
 
 const PopUpContent = ({ type, data, onClose }) => {
   const navigate = useNavigate();
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState("");
 
   // const isReport = type === 'report';
 
@@ -19,64 +19,82 @@ const PopUpContent = ({ type, data, onClose }) => {
   };
 
   switch (type) {
-    case 'upload-picture':
+    case "upload-picture":
       return (
         <>
           <h2>Add your pictures</h2>
-          <p className="hint pink">Explore premium plans to generate special cover with AI</p>
+          <p className="hint pink">
+            Explore premium plans to generate special cover with AI
+          </p>
           <div className="btn-group">
-            <AttachFileBtn variant={1} text={'Upload'} />
-            <ProBtn text={'Generate'} />
+            <AttachFileBtn variant={1} text={"Upload"} />
+            <ProBtn text={"Generate"} />
           </div>
         </>
       );
 
-    case 'upload-track':
+    case "upload-track":
       return (
         <>
           <h2>Add your track</h2>
           <div className="text-box">
-            <p className="hint">The audio file must be MP3 format, up to 50 MB and 128 kbps.</p>
-            <p className="hint pink">Explore premium plans to get better quality for your tracks!</p>
+            <p className="hint">
+              The audio file must be MP3 format, up to 50 MB and 128 kbps.
+            </p>
+            <p className="hint pink">
+              Explore premium plans to get better quality for your tracks!
+            </p>
           </div>
           <div className="btn-group">
-            <AttachFileBtn variant={1} text={'Upload'} type="file" />
-            <AttachFileBtn variant={1} text={'Record'} onClick={() => navigate('/record')} />
+            <AttachFileBtn
+              variant={1}
+              text={"Upload"}
+              type="file"
+              accept=".mp3,audio/mpeg"
+            />
+            <AttachFileBtn
+              variant={1}
+              text={"Record"}
+              onClick={() => navigate("/record")}
+            />
           </div>
         </>
       );
 
-    case 'upload-to-editor':
+    case "upload-to-editor":
       return (
         <>
           <h2>Add your track</h2>
           <div className="track-info">
-            <span className="icon">🔴</span> {data?.name || 'MoonshineTry521.mp3'}<br />
-            <small>{data?.length || '4:24'} | {data?.size || '4.1 MB'}</small>
+            <span className="icon">🔴</span>{" "}
+            {data?.name || "MoonshineTry521.mp3"}
+            <br />
+            <small>
+              {data?.length || "4:24"} | {data?.size || "4.1 MB"}
+            </small>
           </div>
           <BasicBtn type="tiny" text="Upload to editor" />
-
         </>
       );
 
-    case 'report':
+    case "report":
       return (
         <>
-          <div className='header'>
+          <div className="header">
             <ReportIcon />
             <h2>Report Form</h2>
           </div>
-          <div className='track-info'>
-            <p className="subtitle">{data?.title || 'Track Title'}</p>
-            <p className="subtitle-author">{data?.title || 'Author'}</p>
+          <div className="track-info">
+            <p className="subtitle">{data?.title || "Track Title"}</p>
+            <p className="subtitle-author">{data?.title || "Author"}</p>
           </div>
           <div className="checkbox-group">
-            <h1 className='report-checkbox'>Report Reason:</h1>
+            <h1 className="report-checkbox">Report Reason:</h1>
             {[
-              'Copyright Infringement',
-              'Inappropriate or Offensive Content',
-              'Technical Issues (low sound quality, errors)',
-              'Other (text field for explanation)'
+              "Copyright Infringement",
+              "Inappropriate or Offensive Content",
+              "Technical Issues (low sound quality, errors)",
+              "Other (text field for explanation)",
             ].map((reason, i) => (
               <label className="custom-checkbox" key={i}>
                 <input
@@ -89,15 +107,16 @@ const PopUpContent = ({ type, data, onClose }) => {
               </label>
             ))}
 
-            {selected.includes('Other') && (
-              <input className="input-text" placeholder="Describe report reason" />
+            {selected.includes("Other") && (
+              <input
+                className="input-text"
+                placeholder="Describe report reason"
+              />
             )}
           </div>
           <div className="btn-group">
-
             <BasicBtn type="tinyOutline" text="Cancel" />
             <BasicBtn type="tiny" text="Send" />
-
           </div>
         </>
       );
